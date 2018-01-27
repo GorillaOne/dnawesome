028cd1d45734fd60fd1cd6426a97c2de2532b425
Jesse Crafts-Finch <gorillaone@gorillatactics.com>
2018-01-26 23:09:06 -0500

    Added a DNAController and DNAExpresser.

---
+++ DNAwesome/Assets/Scripts/DNAExpresser.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DNAwesome
+{
+	public class DNAExpresser : MonoBehaviour
+	{
+
+		public Text A;
+		public Text B;
+		public Text C;
+		public DNAController DNAController;
+
+		// Use this for initialization
+		void Start()
+		{
+			DNAController.DNAUpdated += DNAController_DNAUpdated;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private void DNAController_DNAUpdated(object sender, System.EventArgs e)
+		{
+			var dna = DNAController.DNA;
+			A.text = dna.GeneList[0].AlleleList[0] + "   " + dna.GeneList[0].AlleleList[1];
+			B.text = dna.GeneList[1].AlleleList[0] + "   " + dna.GeneList[1].AlleleList[1];
+			C.text = dna.GeneList[2].AlleleList[0] + "   " + dna.GeneList[2].AlleleList[1];
+		}
+	}
+}
+
